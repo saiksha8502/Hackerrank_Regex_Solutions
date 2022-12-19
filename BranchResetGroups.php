eff215185ad53732855b02f96b7c14ee83acdc04
Saiksha Desai <104218478+saiksha8502@users.noreply.github.com>
2022-12-19 11:09:05 +0530

    Update BranchResetGroups.php

--- BranchResetGroups.php
+++ BranchResetGroups.php
@@ -1,7 +1,5 @@
 <?php
-
 $Regex_Pattern = '/^\d{2}(-(?:--)?|\.|:)\d{2}\1\d{2}\1\d{2}$/'; //Do not delete '/'. Replace __________ with your regex.
-
 $handle = fopen ("php://stdin","r");
 $Test_String = fgets($handle);
 if(preg_match($Regex_Pattern, $Test_String, $output_array)){
@@ -9,6 +7,5 @@ if(preg_match($Regex_Pattern, $Test_String, $output_array)){
 } else {
     print ("false");
 }
-
 fclose($handle);
 ?>
