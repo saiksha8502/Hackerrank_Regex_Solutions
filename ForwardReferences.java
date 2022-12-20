7c5589bd3cd221973dce9d75811bf54eb6e9f37f
Saiksha Desai <104218478+saiksha8502@users.noreply.github.com>
2022-12-20 19:23:33 +0530

    Update ForwardReferences.java

--- ForwardReferences.java
+++ ForwardReferences.java
@@ -3,26 +3,18 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
 public class Solution {    
-
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {     
         Regex_Test tester = new Regex_Test();
         tester.checker("^(\\2tic|(tac))+$");// Use \\ instead of using \ 
-    
     }
 }
-
 class Regex_Test {
-
-    public void checker(String Regex_Pattern){
-    
+    public void checker(String Regex_Pattern){   
         Scanner Input = new Scanner(System.in);
         String Test_String = Input.nextLine();
         Pattern p = Pattern.compile(Regex_Pattern);
         Matcher m = p.matcher(Test_String);
         System.out.println(m.find());
-    }   
-    
+    }       
 }
